3b7242ee48a06d61ffc62d3005f14f46cc73c5c6
lazaralex98 <alex2012ro@gmail.com>
2024-06-26 13:39:25 +0300

    add comments

--- main.go
+++ main.go
@@ -77,23 +77,28 @@ func (n *Node) Edit(data string) {
 }
 
 func main() {
+	// init registry
 	registry := &Registry{}
 	registry.Init()
-	registry.CreateNode("1", "Hello, World!")
 
+	// create node 1
+	registry.CreateNode("1", "Hello, World!")
 	assert(len(registry.Nodes) == 1, "registry has 1 node")
 	assert(registry.Nodes["1"].ID == "1", "node 1 has id 1")
 	assert(registry.Nodes["1"].Data == "Hello, World!", "node 1 has data: Hello, World!")
 
+	// edit node 1
 	registry.Nodes["1"].Edit("Bye, World!")
 	assert(registry.Nodes["1"].Data == "Bye, World!", "node 1 has data: Bye, World!")
 
+	// create node 2
 	node2 := registry.CreateNode("2")
 	node2.Pull()
 	assert(len(registry.Nodes) == 2, "registry has 2 nodes")
 	assert(registry.Nodes["2"].ID == "2", "node 2 has id 2")
 	assert(registry.Nodes["2"].Data == "Bye, World!", "node 2 has data: Bye, World!")
 
+	// edit node 2
 	registry.Nodes["2"].Edit("Hello, World!")
 	for _, node := range registry.Nodes {
 		assert(node.Data == "Hello, World!", "node "+node.ID+" has data: Hello, World!")
